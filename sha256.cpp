d0b35750aab4148e130286fe618d29d769a28e5e
unknown <andreifcoelho@gmail.com>
2022-02-28 13:18:14 -0300

    refatoração

--- sha256.cpp
+++ sha256.cpp
@@ -7,9 +7,7 @@
  * 
  * @details
  *      Este é um código que eu criei do algorítmo 
- *      SHA-256 para fins de estudo do tutorial:
- * 
- *      https://www.youtube.com/watch?v=ElugJOU5XyQ&list=PLqLkgoa8tYcU0NI9WVvX-Qm6Bzh-wvBgE&index=1
+ *      SHA-256 para fins de estudo.
  * 
  *      Apesar de funcionar corretamente, eu não recomendo
  *      substituir este código por bibliotecas já consagradas
@@ -46,6 +44,7 @@ std::bitset<32>(0x391c0cb3), std::bitset<32>(0x4ed8aa4a), std::bitset<32>(0x5b9c
 std::bitset<32>(0x748f82ee), std::bitset<32>(0x78a5636f), std::bitset<32>(0x84c87814), std::bitset<32>(0x8cc70208),
 std::bitset<32>(0x90befffa), std::bitset<32>(0xa4506ceb), std::bitset<32>(0xbef9a3f7), std::bitset<32>(0xc67178f2) };
 
+
 std::vector<std::bitset<32> > H;
 
 template <size_t N1, size_t N2 >
@@ -58,18 +57,16 @@ std::bitset<N> rotate(std::bitset<N> b, unsigned m) {
    b = b >> m | b << (N-m); 
    return b;
 }
-
 bool fullAdder(bool b1, bool b2, bool& carry)
 {
     bool sum = (b1 ^ b2) ^ carry;
     carry = (b1 && b2) || (b1 && carry) || (b2 && carry);
     return sum;
 }
-// Function to add two bitsets
+
 std::bitset<32> bitsetAdd(std::bitset<32> x, std::bitset<32> y)
 {
     bool carry = false;
-    // bitset to store the sum of the two bitsets
     std::bitset<32> ans;
     for (int i = 0; i < 32; i++) {
         ans[i] = fullAdder(x[i], y[i], carry);
@@ -86,6 +83,7 @@ std::bitset<8> char_to_byte(char c){
     return b;
 }
 
+
 void initialize_H(){
     H.push_back(std::bitset<32>(0x6a09e667));
     H.push_back(std::bitset<32>(0xbb67ae85));
@@ -96,6 +94,7 @@ void initialize_H(){
     H.push_back(std::bitset<32>(0x1f83d9ab));
     H.push_back(std::bitset<32>(0x5be0cd19));
 }
+
 
 std::string bytes_to_hex(std::vector<std::bitset<32> > msg_bytes){
 
@@ -116,99 +115,14 @@ std::string bytes_to_hex(std::vector<std::bitset<32> > msg_bytes){
     return rfinal;
 }
 
-/**
- * @brief função para gerar os blocos da mensagem 
- * 
- * @param message 
- * @return const std::vector<std::vector<std::bitset<32> > > 
- */
-const std::vector<std::vector<std::bitset<32> > > message_blocs(std::string message){
-    
-    std::vector<std::vector<std::bitset<32> > > blocs;
-    std::vector<std::string> message_parts;
-    
-    size_t max_size = 56;
-    size_t msg_size = message.length();
-    std::bitset<32> msg_len_bits = std::bitset<32>(msg_size * 8);
-    
-    if(msg_size > max_size){
-        size_t i = 0;
-        while (msg_size > i) {
-            std::string part = message.substr(i, max_size);
-            message_parts.push_back(part);
-            i += max_size;
-        }
-    } else {
-        message_parts.push_back(message);
-    }
-
-    size_t msg_parts_size = message_parts.size();
-    for (size_t y = 0; y < msg_parts_size; y++) {
-        
-        std::vector<std::bitset<32> > bloc;
-        std::string msg_part = message_parts[y];
-        size_t msg_part_len = msg_part.length();
-        
-        std::vector<std::bitset<8> > word; // 4 bytes -> 32 bits
-
-        for (size_t w = 0; w < msg_part_len; w++) {
-            
-            word.push_back(char_to_byte(msg_part[w]));
-
-            if(word.size() == 4){
-                std::bitset<32> _word = concat_bytes(concat_bytes(word[0], word[1]), concat_bytes(word[2], word[3]));
-                bloc.push_back(_word);
-                word.clear();
-            }
-
-        }
-
-        bool one_bit = true;
-
-        if(word.size() > 0){
-            
-            size_t maxR  =  4 - word.size();
-            for (size_t i = 0; i < maxR; i++){
-                if(one_bit){
-                    word.push_back(std::bitset<8>(0x80));
-                    one_bit = false;
-                    continue;
-                }
-                word.push_back(std::bitset<8>(0x0));
-            }
-
-            std::bitset<32> _word = concat_bytes(concat_bytes(word[0], word[1]), concat_bytes(word[2], word[3]));
-            bloc.push_back(_word);
-            word.clear();
-        }
-
-        while (bloc.size() < 14) {
-            
-            if(one_bit){
-                bloc.push_back(std::bitset<32>(0x80000000));
-                one_bit = false;
-                continue;
-            }
-
-            bloc.push_back(std::bitset<32>(0x0));
-        }
-        
-        bloc.push_back(std::bitset<32>(0x0));
-        bloc.push_back(msg_len_bits);
-        blocs.push_back(bloc);
-    }
-    
-    return blocs;
-    
-}
 
 /**
- * @brief função para expandir o bloco de 15 palavras para 64 palavras
- *        usando a seguinte formula
- *        t0 = (w[i-15] >>  7) xor (w[i-15] >> 18) xor (w[i-15] >>>  3)
- *        t1 = (w[i-2]  >> 17) xor (w[i-2]  >> 19) xor (w[i-2]  >>> 10)
- *        w[i] = w[i-16] + w[i-7] + t0 + t1
- * @param bloc 
+ * @brief  função para expandir o bloco de 15 palavras para 64 palavras
+ *         usando a seguinte formula
+ *         t0 = (w[i-15] >>  7) xor (w[i-15] >> 18) xor (w[i-15] >>>  3)
+ *         t1 = (w[i-2]  >> 17) xor (w[i-2]  >> 19) xor (w[i-2]  >>> 10)
+ *         w[i] = w[i-16] + w[i-7] + t0 + t1
+ * @param  bloc 
  * @return std::vector<std::bitset<32> > 
  */
 std::vector<std::bitset<32> > bloc_expansion(std::vector<std::bitset<32> > bloc){
@@ -224,27 +138,132 @@ std::vector<std::bitset<32> > bloc_expansion(std::vector<std::bitset<32> > bloc)
     return bloc;
 }
 
-std::string hash256(std::string message){
+
+/**
+ * @brief função para gerar um bloco da mensagem
+ * 
+ * @param part_message 
+ * @param last_block 
+ * @param endBytes 
+ * @return std::vector<std::bitset<32> > 
+ */
+std::vector<std::bitset<32> > gen_block(std:: string part_message, bool last_block = false, std::bitset<32> endBytes = std::bitset<32> (0x00)){
+    
+    std::bitset<8>  oneByte  = std::bitset<8> (0x80);
+    std::bitset<8>  zeroByte = std::bitset<8> (0x00);
+    size_t msg_size = part_message.length();
+
+
+    size_t max_words = last_block ? 14 : 16;
+    std::vector<std::bitset<8> > words;
+    std::vector<std::bitset<32> > bloc;
+
+    for (size_t i = 0; i < msg_size; i++)
+    {
+       if(words.size() == 4){
+            // gera a palavra de 32 bits e insere no bloco
+            // zera o bloco
+            std::bitset<32> _word = concat_bytes(concat_bytes(words[0], words[1]), concat_bytes(words[2], words[3]));
+            bloc.push_back(_word);
+            words.clear();
+       }
+
+        words.push_back(char_to_byte(part_message[i]));
+    }
+    
+    bool init = true;
+
+    if(words.size() != 0){
+        for (size_t i = 0; i < words.size(); i++)
+        {
+            if(words.size() == 4){
+                std::bitset<32> _word = concat_bytes(concat_bytes(words[0], words[1]), concat_bytes(words[2], words[3]));
+                bloc.push_back(_word);
+                words.clear();
+            }
+            words.push_back((init ? oneByte : zeroByte));
+            init = false;
+        }
+        
+    }
+
+    while (bloc.size() <= max_words)
+    {
+        std::bitset<32> _word_32 = init ? 
+            concat_bytes(concat_bytes(oneByte , zeroByte), concat_bytes(zeroByte, zeroByte)) :
+            concat_bytes(concat_bytes(zeroByte, zeroByte), concat_bytes(zeroByte, zeroByte)) ;
+
+        init = false;
+        bloc.push_back(_word_32);
+    }
+    
+    if(last_block) bloc.push_back(endBytes);
+    return bloc;
+}
+
+
+/**
+ * @brief função para gerar a lista de blocos da mensagem
+ * 
+ * @param message 
+ * @return const std::vector<std::vector<std::bitset<32> > > 
+ */
+std::vector<std::vector<std::bitset<32> > > pre_processing_message(std::string message){
+    
+    std::vector<std::vector<std::bitset<32> > > blocs;
+    std::vector<std::string> message_parts;
+    size_t max_char_bloc = 55;
+    size_t siz_char_bloc = 64;
+    size_t msg_size = message.length();
+    std::bitset<32> endBytes = std::bitset<32>(msg_size * 8);
+
+    if(msg_size > max_char_bloc){
+        size_t i = 0;
+        while (msg_size > i) {
+            std::string part = message.substr(i, max_char_bloc);
+            message_parts.push_back(part);
+            i += max_char_bloc;
+        }
+    } else {
+        message_parts.push_back(message);
+    }
+
+    std::vector<std::bitset<32> > lastBlock = gen_block(message_parts[message_parts.size()-1], true, endBytes);
+    size_t max_blocs = message_parts.size()-1;
+    for (size_t i = 0; i < max_blocs; i++) blocs.push_back(gen_block(message_parts[i]));
+    blocs.push_back(lastBlock);
+
+    return blocs;
+
+}
+
+/**
+ * @brief gera o hash da mensagem
+ * 
+ * @param message 
+ * @return std::string 
+ */
+std::string sha256(std::string message){
     
     initialize_H();
 
-    std::vector<std::vector<std::bitset<32> > > blocs = message_blocs(message);
+    std::vector<std::vector<std::bitset<32> > > blocs = pre_processing_message(message);
 
     for (size_t i = 0; i < blocs.size(); i++){
+       
+        std::bitset<32> a = H[0];
+        std::bitset<32> b = H[1];
+        std::bitset<32> c = H[2];
+        std::bitset<32> d = H[3];
+        std::bitset<32> e = H[4];
+        std::bitset<32> f = H[5];
+        std::bitset<32> g = H[6];
+        std::bitset<32> h = H[7];
         
         std::vector<std::bitset<32> > blocs_ex = bloc_expansion(blocs[i]);
 
         for (size_t y = 0; y < blocs_ex.size(); y++)
         {
-            std::bitset<32> a = H[0];
-            std::bitset<32> b = H[1];
-            std::bitset<32> c = H[2];
-            std::bitset<32> d = H[3];
-            std::bitset<32> e = H[4];
-            std::bitset<32> f = H[5];
-            std::bitset<32> g = H[6];
-            std::bitset<32> h = H[7];
-            
             std::bitset<32> s1 = rotate(e, 6) ^ rotate(e, 11) ^ rotate(e, 25);
             std::bitset<32> ch = (e & f) ^ (~e & g);
             std::bitset<32> t1 = bitsetAdd(bitsetAdd(bitsetAdd(h,s1), bitsetAdd(ch, K[y])),blocs_ex[y]);
@@ -254,33 +273,29 @@ std::string hash256(std::string message){
             std::bitset<32> t2 = bitsetAdd(s0, mj);
 
             h = g; g = f; f = e; e = bitsetAdd(d, t1); d = c; c = b; b = a; a = bitsetAdd(t1,t2);
-
-            H[0] = bitsetAdd(H[0], a);
-            H[1] = bitsetAdd(H[1], b);
-            H[2] = bitsetAdd(H[2], c);
-            H[3] = bitsetAdd(H[3], d);
-            H[4] = bitsetAdd(H[4], e);
-            H[5] = bitsetAdd(H[5], f);
-            H[6] = bitsetAdd(H[6], g);
-            H[7] = bitsetAdd(H[7], h);
-
         }
-        
 
+        H[0] = bitsetAdd(H[0], a);
+        H[1] = bitsetAdd(H[1], b);
+        H[2] = bitsetAdd(H[2], c);
+        H[3] = bitsetAdd(H[3], d);
+        H[4] = bitsetAdd(H[4], e);
+        H[5] = bitsetAdd(H[5], f);
+        H[6] = bitsetAdd(H[6], g);
+        H[7] = bitsetAdd(H[7], h);
+   
     }
-
-    
 
     std::string res = bytes_to_hex(H);
     H.clear();
+
     return res;
 }
 
+
 int main()
 {
-    std::string res = hash256("string mnbsdamn,bdsn,mabf kjhdfjhasdifhklajshfkjlashfjksda kjhadsfjkhasfkjhasdf kjhdasfjkashdf");
-    // 473287f8298dba7163a897908958f7c0eae733e25d2e027992ea2edc9bed2fa8
-    // 473287f8298dba7163a897908958f7c0eae733e25d2e027992ea2edc9bed2fa8
+    std::string res = sha256("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
     std::cout << res << "\n";
     system("pause");
     return 0;
